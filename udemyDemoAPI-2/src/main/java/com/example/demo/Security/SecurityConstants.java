7019c2af784b1abe8c33a402f7eea71d9068ddc3
Tushar More <moretushart33@gmail.com>
2023-12-30 19:21:16 +0530

    Udemy Post API Working

--- udemyDemoAPI-2/src/main/java/com/example/demo/Security/SecurityConstants.java
+++ udemyDemoAPI-2/src/main/java/com/example/demo/Security/SecurityConstants.java
@@ -4,8 +4,12 @@ public class SecurityConstants
 {
     public static final long EXPIRATION_TIME = 864000000;//10 days
 
-    public static final String TOKEN_PREFIX = "THUNDER";
+    public static final String TOKEN_PREFIX = "THUNDER ";
     public static final String SIGN_UP_URL = "/users";
+    
+    public static final String TOKEN_SECRET = "cfiwefjecnsddhuseduo3842384723twyfmwecmwr4r8w9rw30r0w83rwjrwjrwe";
 
 
+    public static final String HEADER_STRING = "Token";
+    public static final String USER_ID = "User_Id";
 }
--- udemyDemoAPI-2/src/main/java/com/example/demo/UdemyDemoApi2Application.java
+++ udemyDemoAPI-2/src/main/java/com/example/demo/UdemyDemoApi2Application.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.ws.SpringApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,10 @@ public class UdemyDemoApi2Application {
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	
+
+	//Instead using @Component in the Context class we used @Bean class
+	@Bean
+	public SpringApplicationContext springApplicationContext(){
+		return new SpringApplicationContext();
+	}
 }
